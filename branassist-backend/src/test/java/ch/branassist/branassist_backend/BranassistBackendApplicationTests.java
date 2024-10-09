9676a2c929c9c3f6135a3f04606bae36e273d7eb
CriminalChicken <y.meichtry@gmx.ch>
2024-10-09 15:37:34 +0200

    changes in backend + added h2 db

---
+++ branassist-backend/src/test/java/ch/branassist/branassist_backend/BranassistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.branassist.branassist_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranassistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
